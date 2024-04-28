8a6006d0db2e73711c89da2293d5d8f2a4083eb1
StasAnanas666 <lapshin-5a@yandex.ru>
2024-04-28 18:51:58 +0300

    add some block and use props

--- src/Main/components/Card.jsx
+++ src/Main/components/Card.jsx
@@ -1,16 +1,24 @@
-const Card = () => {
+const Card = (props) => {
     return (
-        <div className="col-sm-12 col-md-3 col-lg-4">
+        <div className="col-sm-12 col-md-4 col-lg-3">
             <div class="card mb-4">
-                <img src={require("../../assets/images/work2.jpg")} class="card-img-top" alt="..." />
+                <img src={props.image} class="card-img-top" alt={props.title} />
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">{props.title}</h5>
+                    <p class="card-text">Price: {props.price}$</p>
+                    <a href="#" class="btn btn-primary">
+                        Go somewhere
+                    </a>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
+
+Card.defaultProps = {
+    title: "Unknown work",
+    price: 0.0,
+    image: require("../../assets/images/work0.jpg"),
+};
 
 export default Card;
--- src/Main/components/Gallery.jsx
+++ src/Main/components/Gallery.jsx
@@ -1,22 +1,21 @@
 import Card from "./Card";
 
-const Gallery = () => {
+const Gallery = (props) => {
     return (
         <div className="container my-5">
+            <h2 className="text-center mb-4">Gallery</h2>
             <div className="row">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {props.data.map((item) => (
+                    <Card
+                        title={item.title}
+                        price={item.price}
+                        image={item.image}
+                    />
+                ))}
+                <Card />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Gallery;
